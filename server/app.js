72f5bee27a56f7d99eaefacd461acffc5a78845f
pramodsirsath <pramodsirsath1406@gmail.com>
2026-04-05 21:48:51 +0530

    Update CORS configuration to allow all origins

--- server/app.js
+++ server/app.js
@@ -7,7 +7,9 @@ const authRoutes = require("./src/routes/auth.js");
 const builderRoutes = require("./src/routes/builderRoutes.js");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // serve images
